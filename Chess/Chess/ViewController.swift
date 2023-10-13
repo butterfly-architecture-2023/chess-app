8dd6bef1056957a121bfb1ca9630172b1c0935b3
500beckwon <kjsx60@gmail.com>
2023-10-14 00:33:03 +0900

    Step1 Chess Project 생성

---
+++ Chess/Chess/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chess
+//
+//  Created by ByungHoon Ann on 2023/10/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
