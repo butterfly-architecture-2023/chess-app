c7efd8b69d5b66e4bfbe12a3d6b649c7ab8ada1a
khyeji98 <gpwl981207@naver.com>
2023-10-13 23:02:57 +0900

    init project

---
+++ ChessApp/ChessApp/ChessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessAppApp.swift
+//  ChessApp
+//
+//  Created by 김혜지 on 10/13/23.
+//
+
+import SwiftUI
+
+@main
+struct ChessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
