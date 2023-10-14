33203608c741f0e02ac3b4f112720c3aaf36b138
sohee120 <yuhsohee120@gmail.com>
2023-10-14 14:42:46 +0900

    initial commit

---
+++ chess-app/chess-app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  chess-app
+//
+//  Created by 윤소희 on 2023/10/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ chess-app/chess-app/chess_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  chess_appApp.swift
+//  chess-app
+//
+//  Created by 윤소희 on 2023/10/14.
+//
+
+import SwiftUI
+
+@main
+struct chess_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
