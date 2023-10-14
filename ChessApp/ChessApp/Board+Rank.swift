53f0ded7b6a9727eb73e5bbbc4a2f2144ac8fce0
khyeji98 <gpwl981207@naver.com>
2023-10-15 03:47:13 +0900

    feat: Rank enum 객체 및 케이스 정의
    - Board 가로 행을 나타내는 객체

---
+++ ChessApp/ChessApp/Board+Rank.swift
@@ -0,0 +1,19 @@
+//
+//  Board+Rank.swift
+//  ChessApp
+//
+//  Created by 김혜지 on 10/15/23.
+//
+
+extension Board {
+    enum Rank {
+        case one
+        case two
+        case three
+        case four
+        case five
+        case six
+        case seven
+        case eight
+    }
+}
