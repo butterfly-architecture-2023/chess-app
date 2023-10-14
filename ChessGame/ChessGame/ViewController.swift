1a272e26f392bf9750106f0f7e5f0c5ad786e293
camosss <camosss@naver.com>
2023-10-14 23:05:26 +0900

    Initial Commit

---
+++ ChessGame/ChessGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChessGame
+//
+//  Created by 강호성 on 10/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
