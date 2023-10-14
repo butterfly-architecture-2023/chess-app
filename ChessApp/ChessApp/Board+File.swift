c95e7268d4b7ec8bb424ed785537d32b018f6625
khyeji98 <gpwl981207@naver.com>
2023-10-15 03:49:58 +0900

    feat: File enum 객체 및 케이스 정의
    - Board 세로 열을 의미하는 객체

---
+++ ChessApp/ChessApp/Board+File.swift
@@ -0,0 +1,19 @@
+//
+//  Board+File.swift
+//  ChessApp
+//
+//  Created by 김혜지 on 10/15/23.
+//
+
+extension Board {
+    enum File {
+        case a
+        case b
+        case c
+        case d
+        case e
+        case f
+        case g
+        case h
+    }
+}
