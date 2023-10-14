ba25d02968f475afbd6a53af67d73b065ffe6d6a
jiyeon <jiyeonlab@gmail.com>
2023-10-15 00:06:07 +0900

    feat: 프로젝트 생성

---
+++ ChessApp/ChessApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChessApp
+//
+//  Created by Jiyeon  on 2023/10/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
