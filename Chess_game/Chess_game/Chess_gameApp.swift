41d2fc9a19ad36d95dbf0aad327e55a1170cc234
Piggy <pigbag00@gmail.com>
2023-10-15 10:45:34 +0900

    init project (#2)
    Co-authored-by: piggy.seob <www.piggy.park@groundx.xyz>

---
+++ Chess_game/Chess_game/Chess_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chess_gameApp.swift
+//  Chess_game
+//
+//  Created by 박진섭 on 10/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Chess_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
