8b33cccf0d901a87cbc3c2031527886ee83f58ef
piggy.seob <www.piggy.park@groundx.xyz>
2023-10-15 20:10:00 +0900

    add Board && Pawn
     Changes to be committed:
    	modified:   Chess_game/Chess_game.xcodeproj/project.pbxproj
    	new file:   Chess_game/Chess_game/Board.swift
    	new file:   Chess_game/Chess_game/Pawn.swift

---
+++ Chess_game/Chess_game/Board.swift
@@ -0,0 +1,12 @@
+//
+//  Board.swift
+//  Chess_game
+//
+//  Created by 박진섭 on 10/15/23.
+//
+
+import Foundation
+
+struct Board {
+
+}
---
+++ Chess_game/Chess_game/Pawn.swift
@@ -0,0 +1,12 @@
+//
+//  Pawn.swift
+//  Chess_game
+//
+//  Created by 박진섭 on 10/15/23.
+//
+
+import Foundation
+
+struct Pawn {
+    
+}
