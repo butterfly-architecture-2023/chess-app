2d11dbfd3410c15647ed5b8f4873929a82b08ca0
Sanghee <dev.sanghee@gmail.com>
2023-10-15 10:18:39 +0900

    feat: 프로젝트 추가

---
+++ ChessGame/ChessGame/ChessGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessGameApp.swift
+//  ChessGame
+//
+//  Created by 안상희 on 2023/10/15.
+//
+
+import SwiftUI
+
+@main
+struct ChessGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
