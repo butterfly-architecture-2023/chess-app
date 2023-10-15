f824a713114b21f38234f2c1360ddad95e1ef4ab
ly__ <57200871+lxxyeon@users.noreply.github.com>
2023-10-15 23:38:41 +0900

    Project Setting (#14)

---
+++ ChessApp/ChessApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChessApp
+//
+//  Created by leeyeon2 on 10/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
