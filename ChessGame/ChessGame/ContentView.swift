90113d528b486ac13286286c2d4345eb455b9a1e
Sanghee <95578975+AhnSangHee@users.noreply.github.com>
2023-10-15 10:42:48 +0900

    feat: 프로젝트 추가 (#11)

---
+++ ChessGame/ChessGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChessGame
+//
+//  Created by 안상희 on 2023/10/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
