c0b32a0ab9c91f8f6993fd36e0bbb0ee872b1454
Song Jiyeon <jiyeon.song@riiid.co>
2023-10-15 23:39:05 +0900

    ✨ Create project (#16)

---
+++ ChessApp/ChessApp/ChessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessAppApp.swift
+//  ChessApp
+//
+//  Created by 송지연 on 10/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ChessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChessApp/ChessApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChessApp
+//
+//  Created by 송지연 on 10/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
