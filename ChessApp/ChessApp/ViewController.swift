545d4d13f3cde7ec458168d0315fd7751cba45a2
KYHyeon <run190822@gmail.com>
2023-10-15 23:38:54 +0900

    Init ChessApp project (#15)

---
+++ ChessApp/ChessApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChessApp
+//
+//  Created by 현기엽 on 2023/10/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
