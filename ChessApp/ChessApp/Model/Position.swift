de5fea93675b0e248a425a485c978f191a259c0d
jiyeon <jiyeonlab@gmail.com>
2023-10-17 01:29:00 +0900

    fix: 불필요한 코드 제거

--- ChessApp/ChessApp/Model/Position.swift
+++ ChessApp/ChessApp/Model/Position.swift
@@ -18,16 +18,4 @@ struct Position {
     
     let rank: Rank
     let file: File
-    
-    // 초기화가 가능한 Position인지
-    func isPossibleInitPosition() -> Bool {
-        var isPossible: Bool = false
-        switch rank {
-        case .one, .two, .seven, .eight:
-            isPossible = true
-        default:
-            break
-        }
-        return isPossible
-    }
 }
