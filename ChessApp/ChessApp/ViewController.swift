03d9871e4334f01f71ad97cdbb39e2fda8f8cb25
austin.kd <austin.kd@kakaocorp.com>
2023-10-16 15:58:05 +0900

    initiate commit

---
+++ ChessApp/ChessApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChessApp
+//
+//  Created by 김도형 on 16/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
