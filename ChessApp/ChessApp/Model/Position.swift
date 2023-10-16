67c13c3237f590b7c099fd9d63316fd62cd816a9
jiyeon <jiyeonlab@gmail.com>
2023-10-17 00:39:47 +0900

    feat: 초기화가 가능한 Position인지 확인하는 로직 추가

--- ChessApp/ChessApp/Model/Position.swift
+++ ChessApp/ChessApp/Model/Position.swift
@@ -16,6 +16,18 @@ struct Position {
         case A, B, C, D, E, F, G, H
     }
     
-    var rank: Rank
-    var file: File
+    let rank: Rank
+    let file: File
+    
+    // 초기화가 가능한 Position인지
+    func isPossibleInitPosition() -> Bool {
+        var isPossible: Bool = false
+        switch rank {
+        case .one, .two, .seven, .eight:
+            isPossible = true
+        default:
+            break
+        }
+        return isPossible
+    }
 }
