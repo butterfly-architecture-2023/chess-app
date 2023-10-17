735de18b2620928393c91e7657ffe9dded2dacf5
sohee120 <yuhsohee120@gmail.com>
2023-10-17 22:08:15 +0900

    add: 게임점수 초기화 함수

--- chess-app/chess-app/Board.swift
+++ chess-app/chess-app/Board.swift
@@ -16,10 +16,15 @@ class Board {
     var board = Array(repeating: Array(repeating: ".", count: 8), count:8)
     
     func resetBoard() {
-            board = Array(repeating: Array(repeating: ".", count: 8), count:8)
-            board[1] = Array(repeating: blackPawn, count: 8)
-            board[6] = Array(repeating: whitePawn, count: 8)
-        }
+        board = Array(repeating: Array(repeating: ".", count: 8), count:8)
+        board[1] = Array(repeating: blackPawn, count: 8)
+        board[6] = Array(repeating: whitePawn, count: 8)
+    }
+    
+    func resetScore() {
+        blackScore = 8
+        whiteScore = 8
+    }
     
 }
 
