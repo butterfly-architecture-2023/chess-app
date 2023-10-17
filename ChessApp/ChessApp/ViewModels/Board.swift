50ef6f9538b9bf389a0deb690142a203263793fe
Songjiyeon <carol.jiyeon.song@gmail.com>
2023-10-18 00:57:05 +0900

    👔 Add initialSquares method to initialize squares

--- ChessApp/ChessApp/ViewModels/Board.swift
+++ ChessApp/ChessApp/ViewModels/Board.swift
@@ -2,4 +2,25 @@ import Foundation
 
 class Board {
   init() {}
+
+  typealias Squares = [Int: [String: Piece?]]
+
+  var squares: Squares = [:]
+
+  func initialSquares() -> Squares {
+    var result: [Int: [String: Piece?]] = [:]
+
+    Array(0...7).forEach { rank in
+      Array(0...7).forEach { index in
+        result[rank + 1]?[String(format: "%c", index + 65)] = {
+          guard rank == 1 || rank == 6 else {
+            return nil
+          }
+          return Piece(pieceType: .pawn, colorType: rank == 1 ? .black : .white)
+        }()
+      }
+    }
+
+    return result
+  }
 }
