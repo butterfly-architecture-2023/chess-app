fcaabd12f4b0f2e391ab0fefbf66e94e6ec7e230
Lee SeEun <sally4405@naver.com>
2023-10-18 00:12:01 +0900

    feat: init project

---
+++ ChessApp/ChessApp/ChessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessAppApp.swift
+//  ChessApp
+//
+//  Created by Se Eun Lee on 2023/10/18.
+//
+
+import SwiftUI
+
+@main
+struct ChessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
