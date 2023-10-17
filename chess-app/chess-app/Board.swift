c728a15f537f1e7b7d77e3467c062771f777c18e
sohee120 <yuhsohee120@gmail.com>
2023-10-17 22:33:54 +0900

    add: 폰 옮기는 함수

--- chess-app/chess-app/Board.swift
+++ chess-app/chess-app/Board.swift
@@ -26,5 +26,54 @@ class Board {
         whiteScore = 8
     }
     
+    func movePawn(from:String, to:String, turn:Int) -> Bool {
+        
+        var isPossible: Bool = true
+        
+        let inputFrom = from.split(separator: "")
+        let inputTo = to.split(separator: "")
+        let fromFile = Int(UnicodeScalar(String(inputFrom[0]))!.value)-65
+        let toFile = Int(UnicodeScalar(String(inputTo[0]))!.value)-65
+        let fromRank = Int(inputFrom[1])! - 1
+        let toRank = Int(inputTo[1])! - 1
+        
+        //입력값 경계 설정
+        if fromFile < 0 || fromFile > 7 || fromRank < 0 || fromRank > 7 || toFile < 0 || toFile > 7 || toRank < 0 || toRank > 7 {
+            return false
+        }
+        
+        //가로로 움직일 수 없도록
+        if fromFile != toFile {
+            return false
+        }
+        
+        if turn % 2 == 1 {// 흑색말
+            //같은색일경우
+            if board[toRank][toFile] == "\u{265F}" {
+                return false
+            } else if board[toRank][toFile] == "\u{2659}" {
+                board[toRank][toFile] = "\u{265F}"
+            }
+            //작은 rank에서 더 큰 rank로 움직이도록
+            if toRank - fromRank != 1 {
+                return false
+            }
+        }
+        
+        if turn % 2 == 0 {// 백색말
+            //같은색일경우
+            if board[toRank][toFile] == "\u{2659}" {
+                return false
+            } else if board[toRank][toFile] == "\u{265F}" {
+                board[toRank][toFile] = "\u{2659}"
+            }
+            //큰 rank에서 더 작은 rank로 움직이도록
+            if fromRank - toRank != 1 {
+                return false
+            }
+        }
+        
+        return isPossible
+    }
 }
 
