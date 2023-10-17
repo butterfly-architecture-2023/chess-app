476d4b07804096b220733c241bf54ff295a316f3
Song-e Han <se.han@dramancompany.com>
2023-10-17 21:02:34 +0900

    feat: create a project

---
+++ SwiftChessApp/SwiftChessApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftChessApp
+//
+//  Created by Han Songe on 2023/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
