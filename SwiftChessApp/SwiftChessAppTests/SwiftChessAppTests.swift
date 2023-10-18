8591fbadf3cd7661b0cc9d0f08c599ab9788a188
Song-e Han <se.han@dramancompany.com>
2023-10-18 20:19:41 +0900

    test: 흑백 Pawn 초기 생성 위치에 맞는 체스말인지 테스트하는 함수 추가

--- SwiftChessApp/SwiftChessAppTests/SwiftChessAppTests.swift
+++ SwiftChessApp/SwiftChessAppTests/SwiftChessAppTests.swift
@@ -19,4 +19,12 @@ final class SwiftChessAppTests: XCTestCase {
     override func tearDownWithError() throws {
         board = nil
     }
+    
+    func test_보드생성_흑pawn_초기생성위치() {
+        XCTAssertEqual(board.getPiece(at: ChessSquare(fileIndex: 3, rankIndex: 1))?.color, .black)
+    }
+    
+    func test_보드생성_백pawn_초기생성위치() {
+        XCTAssertEqual(board.getPiece(at: ChessSquare(fileIndex: 1, rankIndex: 6))?.color, .white)
+    }
 }
