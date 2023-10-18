0e17df966d40ea3bc105745ab45bdc581f537a07
sello (sally) <45891045+sally4405@users.noreply.github.com>
2023-10-18 11:57:53 +0900

    feat: init project (#24)

---
+++ ChessApp/ChessApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChessApp
+//
+//  Created by Se Eun Lee on 2023/10/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
