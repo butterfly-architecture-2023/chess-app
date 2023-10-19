a00d7c68cc2100d67cad978d027fd8ed30c10657
lxxyeon <lxxyeon@gmail.com>
2023-10-19 13:20:26 +0900

    불필요한 주석, 메소드 제거

--- ChessApp/ChessApp/ViewController.swift
+++ ChessApp/ChessApp/ViewController.swift
@@ -15,51 +15,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let board = Board()
-    
-//        // 1. 입력
-//        inputCurrent = "A7"
-//        inputNext = "A6"
-//        print(board.moveToNext(current: inputCurrent, next: inputNext))
-//        board.printBoard()
-//        
-//        inputCurrent = "A6"
-//        inputNext = "A5"
-//        print(board.moveToNext(current: inputCurrent, next: inputNext))
-//        board.printBoard()
-//        
-//        inputCurrent = "A2"
-//        inputNext = "A5"
-//        print(board.moveToNext(current: inputCurrent, next: inputNext))
-//        board.printBoard()
-//        
-//        inputCurrent = "A2"
-//        inputNext = "A3"
-//        print(board.moveToNext(current: inputCurrent, next: inputNext))
-//        board.printBoard()
-//        
-//        inputCurrent = "A6"
-//        inputNext = "A5"
-//        print(board.moveToNext(current: inputCurrent, next: inputNext))
-//        board.printBoard()
-//        
-//        inputCurrent = "A3"
-//        inputNext = "A4"
-//        print(board.moveToNext(current: inputCurrent, next: inputNext))
-//        board.printBoard()
-//        
-//        inputCurrent = "A3"
-//        inputNext = "A4"
-//        print(board.moveToNext(current: inputCurrent, next: inputNext))
-//        board.printBoard()
-//        
-//        inputCurrent = "C7"
-//        inputNext = "C6"
-//        print(board.moveToNext(current: inputCurrent, next: inputNext))
-//        board.printBoard()
-//        
-//        inputCurrent = "A4"
-//        inputNext = "A5"
-//        print(board.moveToNext(current: inputCurrent, next: inputNext))
-//        board.printBoard()
+
     }
 }
