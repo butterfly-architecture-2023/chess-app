23118b3b56cffe00dcc361efddc613fc1a2b457d
Hosung Kang <74236080+camosss@users.noreply.github.com>
2023-10-20 10:15:05 +0900

    체스보드와 PawnⓂ️ (#32)
    * feat: ChessPiece 생성
    
    * feat: 보드 초기 위치 검증 테스트 코드
    
    * feat: 체스말 움직임 검증 테스트 코드
    
    * delete: print문 제거

--- ChessGame/ChessGameTests/ChessGameTests.swift
+++ ChessGame/ChessGameTests/ChessGameTests.swift
@@ -10,27 +10,31 @@ import XCTest
 
 final class ChessGameTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    private var sut: ChessBoard!
+
+    override func setUp() {
+        sut = ChessBoard()
+        super.setUp()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func test_checkInitialPosition() {
+        XCTAssertTrue(sut.checkInitialPosition())
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_black_movePiece() {
+        let from = Pawn(pieceType: .black, position: Position(rank: 2, file: .D))
+        let to = Pawn(pieceType: .black, position: Position(rank: 3, file: .E))
+        XCTAssertTrue(sut.movePiece(from: from, to: to))
     }
 
+    func test_white_movePiece() {
+        let from = Pawn(pieceType: .white, position: Position(rank: 7, file: .D))
+        let to = Pawn(pieceType: .white, position: Position(rank: 6, file: .E))
+        XCTAssertTrue(sut.movePiece(from: from, to: to))
+    }
 }
