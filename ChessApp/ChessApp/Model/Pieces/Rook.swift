107aa3df3e017f064a0630b2f3569a5dbdc5e0b3
KYHyeon <run190822@gmail.com>
2023-10-21 15:08:42 +0900

    [Rook] struct 생성 및 테스트 코드 작성

---
+++ ChessApp/ChessApp/Model/Pieces/Rook.swift
@@ -0,0 +1,23 @@
+//
+//  Rook.swift
+//  ChessApp
+//
+//  Created by 현기엽 on 2023/10/21.
+//
+
+import Foundation
+
+struct Rook: Piece {
+    let color: Color
+    
+    func availableMovePositions(for position: Position) -> Set<Position> {
+        []
+    }
+    
+    let maximumCount: Int = 2
+    let score: Int = 5
+}
+
+extension Rook: CustomStringConvertible {
+    var description: String { "" }
+}
---
+++ ChessApp/ChessAppTests/Model/Pieces/RookTests.swift
@@ -0,0 +1,29 @@
+//
+//  RookTests.swift
+//  ChessAppTests
+//
+//  Created by 현기엽 on 2023/10/21.
+//
+
+import XCTest
+@testable import ChessApp
+
+final class RookTests: XCTestCase {
+    func testD5에_있는_흰색_룩의_움직일_수_있는_위치를_확인한다() {
+        let rook = Rook(color: .white)
+        let availablePositions = rook.availableMovePositions(for: "D5")
+        XCTAssertEqual(availablePositions, [
+            "A5", "B5", "C5", "E5", "F5", "G5", "H5",
+            "D1", "D2", "D3", "D4", "D6", "D7", "D8"
+        ])
+    }
+    
+    func testH8에_있는_검정색_룩의_움직일_수_있는_위치를_확인한다() {
+        let rook = Rook(color: .black)
+        let availablePositions = rook.availableMovePositions(for: "H8")
+        XCTAssertEqual(availablePositions, [
+            "A8", "B8", "C8", "D8", "E8", "F8", "G8",
+            "H1", "H2", "H3", "H4", "H5", "H6", "H7"
+        ])
+    }
+}
