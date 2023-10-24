987bb2be9835e19ab1d5875f2be1c52c7fc63bed
HongzCloud <jihong21511@gmail.com>
2023-10-25 04:13:13 +0900

    :zap: Board 프로퍼티 변경 및 프로그래밍 요구사항 3번까지 수정

--- ChessGame/ChessGame/Board.swift
+++ ChessGame/ChessGame/Board.swift
@@ -9,34 +9,31 @@ import Foundation
 
 final class Board {
     
-    private(set) var square: [[Piece]]
+    private var pieces: [Position: Piece]
     
-    init(square: [[Piece]]) {
-        self.square = square
+    init(pieces: [Position: Piece]) {
+        self.pieces = pieces
     }
     
     convenience init() {
-        self.init(square: Array(repeating: Array(repeating: Piece(category: .empty), count: 8), count: 8))
+        self.init(pieces: [:])
     }
     
     // 프로그래밍 요구사항 1. 체스말(Piece) 존재 여부
     var hasPieces: Bool {
-        square.flatMap { $0 }.contains{ $0.category != .empty }
+        return !pieces.isEmpty
     }
     
     // 프로그래밍 요구사항 2. 흑과 백 점수를 출력
-    func score() -> (blackPawn: Int, whitePawn: Int) {
+    func score() -> (black: Int, white: Int) {
         
-        var score = (blackPawn: 0, whitePawn: 0)
+        var score = (black: 0, white: 0)
         
-        square.flatMap{ $0 }.forEach { piece in
-            if piece.category == .pawn(color: .black) {
-                score.blackPawn += 1
-            } else if piece.category == .pawn(color: .white) {
-                score.whitePawn += 1
-            }
+        pieces.forEach { (position, piece) in
+            let pieceType = type(of: piece)
+            pieceType.color == .black ? (score.black += pieceType.score) : (score.white += pieceType.score)
         }
-        
+
         return score
     }
     
@@ -45,8 +42,13 @@ final class Board {
     // 흑색 Pawn는 ♟ U+265F를 빈 곳은 "."을 표시한다.
     // 백색 Pawn는 ♙ U+2659를 빈 곳은 "."을 표시한다.
     func display() -> [[String]] {
-        let convertedSquare = square.map{ $0.map{ $0.category.unicode } }
-        return convertedSquare
+        var square = Array(repeating: Array(repeating: ".", count: 8), count: 8)
+        
+        pieces.forEach { (position, piece) in
+            square[position.file][position.rank] = piece.description
+        }
+        
+        return square
     }
     
     // 프로그래밍 요구사항 4.
@@ -59,11 +61,11 @@ final class Board {
     func gameStart() {
         
         // 초기화
-        self.square =  Array(repeating: Array(repeating: Piece(category: .empty), count: 8), count: 8)
-        
-        // 폰들 초기화
-        square[1] = square[1].map{ _ in Piece(category: .pawn(color: .black)) }
-        square[6] = square[6].map{ _ in Piece(category: .pawn(color: .white)) }
+//        self.square =  Array(repeating: Array(repeating: Piece(category: .empty), count: 8), count: 8)
+//        
+//        // 폰들 초기화
+//        square[1] = square[1].map{ _ in Piece(category: .pawn(color: .black)) }
+//        square[6] = square[6].map{ _ in Piece(category: .pawn(color: .white)) }
     }
     
     // 프로그래밍 요구사항 5.
@@ -72,33 +74,42 @@ final class Board {
     // 말을 옮길 수 있으면 true, 옮길 수 없으면 false를 리턴한다.
     // 만약, 다른 색상의 말이 to 위치에 있는 경우는 기존에 있던 말을 제거하고 이동한다.
     func movePawn(current: Position) -> Bool {
-        let piece = square[current.rank][current.file]
-        let newRank: Int
-        
-        switch piece.category {
-        case .pawn(color: .black):
-            newRank = current.rank + 1
-        case .pawn(color: .white):
-            newRank = current.rank - 1
-        case .empty:
-            return false
-        }
-        
-        guard isValidMove(piece: piece, position: Position(file: current.file, rank: newRank)) else {
-            return false
-        }
-        
-        square[newRank][current.file] = piece
-        square[current.rank][current.file] = Piece(category: .empty)
+//        let piece = square[current.rank][current.file]
+//        let newRank: Int
+//        
+//        switch piece.category {
+//        case .pawn(color: .black):
+//            newRank = current.rank + 1
+//        case .pawn(color: .white):
+//            newRank = current.rank - 1
+//        case .empty:
+//            return false
+//        }
+//        
+//        guard isValidMove(piece: piece, position: Position(file: current.file, rank: newRank)) else {
+//            return false
+//        }
+//        
+//        square[newRank][current.file] = piece
+//        square[current.rank][current.file] = Piece(category: .empty)
 
         return true
     }
 
     private func isValidMove(piece: Piece, position: Position) -> Bool {
-        guard position.rank >= 0,
-              position.rank < square.count,
-              square[position.rank][position.file].category != piece.category else { return false }
+//        guard position.rank >= 0,
+//              position.rank < square.count,
+//              square[position.rank][position.file].category != piece.category else { return false }
         
         return true
+    }
+}
+
+struct Square {
+    private var pieces: [Position: Piece] = [:]
+    private var map: [[Piece?]]
+
+    init(column: Int, row: Int) {
+        self.map = Array(repeating: Array(repeating: nil, count: 8), count: 8)
     }
 }
