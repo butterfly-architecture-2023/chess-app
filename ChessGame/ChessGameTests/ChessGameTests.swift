fc1f3ad00c50ddc36ea066d17205ac9b5e70cff4
camosss <camosss@naver.com>
2023-10-25 01:23:27 +0900

    feat: chessBoard display 로직 및 각 rank별 문자열을 리턴하는지 테스트 코드 작성

--- ChessGame/ChessGameTests/ChessGameTests.swift
+++ ChessGame/ChessGameTests/ChessGameTests.swift
@@ -21,4 +21,23 @@ final class ChessGameTests: XCTestCase {
         sut = nil
         super.tearDown()
     }
+
+    /// display() 함수는 1-rank부터 8-rank까지 rank 문자열 배열로 보드 위에 체스말을 리턴한다.
+    func test_displayReturnsChessBoardState() {
+        let display = sut.display()
+        let displayLines = display.components(separatedBy: "\n").dropLast()
+
+        XCTAssertEqual(displayLines.count, 8)
+
+        for rank in 0..<displayLines.count {
+            let rankString = getExpectedRankString(forRank: Rank.allCases[rank])
+            XCTAssertEqual(displayLines[rank], rankString)
+        }
+    }
+
+    private func getExpectedRankString(forRank rank: Rank) -> String {
+        let rankString = "\(rank.rawValue)"
+        let pieces = sut.board[rank.rawValue - 1].compactMap { $0?.icon ?? "." }
+        return rankString + pieces.joined()
+    }
 }
--- ChessGame/ChessTest/ChessBoard.swift
+++ ChessGame/ChessTest/ChessBoard.swift
@@ -11,9 +11,64 @@ final class ChessBoard {
 
     // MARK: - Property
 
+    var board: [[Piece?]]
+
     // MARK: - Init
 
+    /// 체스 프로그램을  초기화
     init() {
+        board = Array(
+            repeating: Array(
+                repeating: nil,
+                count: File.allCases.count
+            ),
+            count: Rank.allCases.count
+        )
 
+        for file in File.allCases {
+            board[Rank.two.rawValue - 1][file.rawValue] = Pawn(color: .black)
+        }
+        board[Rank.one.rawValue - 1] = [
+            Rook(color: .black),
+            Knight(color: .black),
+            Bishop(color: .black),
+            nil,
+            Queen(color: .black),
+            Bishop(color: .black),
+            Knight(color: .black),
+            Rook(color: .black)
+        ]
+
+        for file in File.allCases {
+            board[Rank.seven.rawValue - 1][file.rawValue] = Pawn(color: .white)
+        }
+        board[Rank.eight.rawValue - 1] = [
+            Rook(color: .white),
+            Knight(color: .white),
+            Bishop(color: .white),
+            nil,
+            Queen(color: .white),
+            Bishop(color: .white),
+            Knight(color: .white),
+            Rook(color: .white)
+        ]
+    }
+
+    // MARK: - Helpers
+
+    /// 1-rank부터 8-rank까지 rank 문자열로 보드 위에 체스말을 리턴
+    func display() -> String {
+        var displayBoard = ""
+        for rank in Rank.allCases {
+            displayBoard += getRankString(forRank: rank)
+        }
+        print(displayBoard)
+        return displayBoard
+    }
+
+    private func getRankString(forRank rank: Rank) -> String {
+        let rankString = "\(rank.rawValue)"
+        let pieces = board[rank.rawValue - 1].compactMap { $0?.icon ?? "." }
+        return rankString + pieces.joined() + "\n"
     }
 }
