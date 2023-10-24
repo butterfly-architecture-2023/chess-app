1ab9ea2507f4463f0952e471fdbf907daa762a27
HongzCloud <jihong21511@gmail.com>
2023-10-25 04:10:11 +0900

    :art: Position 이니셜라이저에서 입력값 검증하도록 변경

--- ChessGame/ChessGame/Position.swift
+++ ChessGame/ChessGame/Position.swift
@@ -7,7 +7,36 @@
 
 import Foundation
 
-struct Position: Equatable {
-    let file: Int
-    let rank: Int
+struct Position: Equatable, Hashable {
+    
+    // [입력값: file, rank index]
+    static let fileMap: [Character: Int] = ["A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7]
+    static let rankMap = [1: 0, 2: 1, 3: 2, 4: 3, 5: 4, 6: 5, 7: 6, 8: 7]
+    
+    let file: Int     // column
+    let rank: Int     // row
+
+    init?(file: Int, rank: Int) {
+        guard Position.fileMap.keys.count > file,
+              Position.rankMap.keys.count > rank else { return nil }
+        
+        self.file = file
+        self.rank = rank
+    }
+    
+    init?(file: Character, rank: Int) {
+        guard let file = Position.fileMap[file],
+              let rank = Position.rankMap[rank] else { return nil }
+        
+        self.init(file: file, rank: rank)
+    }
+    
+    func topPosition() -> Self? {
+        Position(file: file, rank: rank-1)
+    }
+    
+    func bottomPosition() -> Self? {
+        Position(file: file, rank: rank+1)
+    }
+
 }
