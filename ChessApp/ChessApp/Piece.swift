18e9685812455971666f4fabb2caae2e43e43981
dely <lhyal2p@gmail.com>
2023-10-25 09:54:19 +0900

    Piece Type Bishop, Rook 추가 및 Position 이동 로직 추가

--- ChessApp/ChessApp/Piece.swift
+++ ChessApp/ChessApp/Piece.swift
@@ -9,13 +9,15 @@ import Foundation
 
 protocol Piece {
     // 점수, color, 생성위치, 움직일 수 있는 위치
+    var pieceType: PieceType { get}
     var point: Int { get }
     var pieceColorType: PieceColorType { get }
     func showIcon() -> String
-    func movablePosition() -> String
+    func movablePosition(_ currentPosition: Position) -> [Position]
 }
 
 class Pawn: Piece {
+    var pieceType: PieceType = .pawn
     var point: Int = 1
     var pieceColorType: PieceColorType
     
@@ -27,12 +29,51 @@ class Pawn: Piece {
         return pieceColorType == .black ? "♟" : "♙"
     }
     
-    func movablePosition() -> String {
-        return ""
+    func movablePosition(_ currentPosition: Position) -> [Position] {
+        print(currentPosition.rank, currentPosition.file)
+        return [currentPosition.top(), currentPosition.bottom(),
+                currentPosition.left(), currentPosition.right()]
+    }
+}
+
+class Bishop: Piece {
+    var pieceType: PieceType = .bishop
+    var point: Int = 3
+    var pieceColorType: PieceColorType
+    
+    init(pieceColorType: PieceColorType) {
+        self.pieceColorType = pieceColorType
+    }
+    
+    func showIcon() -> String {
+        return pieceColorType == .black ? "♝" : "♗"
+    }
+    
+    func movablePosition(_ currentPosition: Position) -> [Position] {
+        return currentPosition.diagonal()
+    }
+}
+
+class Rook: Piece {
+    var pieceType: PieceType = .rook
+    var point: Int = 5
+    var pieceColorType: PieceColorType
+    
+    init(pieceColorType: PieceColorType) {
+        self.pieceColorType = pieceColorType
+    }
+    
+    func showIcon() -> String {
+        return pieceColorType == .black ? "♜" : "♖"
+    }
+    
+    func movablePosition(_ currentPosition: Position) -> [Position] {
+        return currentPosition.topList() + currentPosition.bottomList() + currentPosition.leftList() + currentPosition.rightList()
     }
 }
 
 class EmptySpace: Piece {
+    var pieceType: PieceType = .empty
     var point: Int = 0
     var pieceColorType: PieceColorType = .white
     
@@ -40,7 +81,14 @@ class EmptySpace: Piece {
         return "."
     }
     
-    func movablePosition() -> String {
-        return ""
+    func movablePosition(_ currentPosition: Position) -> [Position] {
+        return [Position(rank: 0, file: 0)]
     }
+}
+
+enum PieceType {
+    case empty
+    case pawn
+    case bishop
+    case rook
 }
--- ChessApp/ChessApp/Position.swift
+++ ChessApp/ChessApp/Position.swift
@@ -22,6 +22,114 @@ class Position: Hashable {
         self.init(rank: rankNumber, file: fileNumber)
     }
     
+    func toString() -> String {
+        return String(UnicodeScalar((file + 1) + 64) ?? .init(0)) + String(rank+1)
+    }
+    
+    func top() -> Position {
+        return Position(rank: rank - 1, file: file)
+    }
+    
+    func topList() -> [Position] {
+        var array = [Position(rank: rank, file: file)]
+        for _ in 0..<rank {
+            array.append(array.last!.top())
+        }
+        return array
+    }
+    
+    func bottom() -> Position {
+        return Position(rank: rank + 1, file: file)
+    }
+    
+    func bottomList() -> [Position] {
+        var array = [Position(rank: rank, file: file)]
+        for _ in 0..<(8-rank) {
+            array.append(array.last!.bottom())
+        }
+        return array
+    }
+    
+    func left() -> Position {
+        return Position(rank: rank, file: file - 1)
+    }
+    
+    func leftList() -> [Position] {
+        var array = [Position(rank: rank, file: file)]
+        for _ in 0..<file {
+            array.append(array.last!.left())
+        }
+        return array
+    }
+    
+    func right() -> Position {
+        return Position(rank: rank, file: file + 1)
+    }
+    
+    func rightList() -> [Position] {
+        var array = [Position(rank: rank, file: file)]
+        for _ in 0..<(8-file) {
+            array.append(array.last!.right())
+        }
+        return array
+    }
+    
+    func topLeft() -> Position {
+        return Position(rank: rank - 1, file: file - 1)
+    }
+    
+    func topLeftList() -> [Position] {
+        var array = [Position(rank: rank, file: file)]
+        let count = (rank < file) ? rank : file
+        for _ in 0..<count {
+            array.append(array.last!.topLeft())
+        }
+        return array
+    }
+    
+    func topRight() -> Position {
+        return Position(rank: rank - 1, file: file + 1)
+    }
+    
+    func topRightList() -> [Position] {
+        var array = [Position(rank: rank, file: file)]
+        let count = (rank < (8-file)) ? rank : (8-file)
+        for _ in 0..<count {
+            array.append(array.last!.topRight())
+        }
+        return array
+    }
+    
+    func bottomLeft() -> Position {
+        return Position(rank: rank + 1, file: file - 1)
+    }
+    
+    func bottomLeftList() -> [Position] {
+        var array = [Position(rank: rank, file: file)]
+        let count = ((8-rank) < file) ? (8-rank) : file
+        for _ in 0..<count {
+            array.append(array.last!.bottomLeft())
+        }
+        return array
+    }
+    
+    func bottomRight() -> Position {
+        return Position(rank: rank + 1, file: file + 1)
+    }
+    
+    func bottomRightList() -> [Position] {
+        var array = [Position(rank: rank, file: file)]
+        let count = ((8-rank) < (8-file)) ? (8-rank) : (8-file)
+        for _ in 0..<count {
+            array.append(array.last!.bottomRight())
+        }
+        return array
+    }
+    
+    func diagonal() -> [Position] {
+        return [topLeft(), topRight(), bottomLeft(), bottomRight()]
+    }
+    
     func hash(into hasher: inout Hasher) {
         hasher.combine(rank)
         hasher.combine(file)
