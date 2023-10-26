f8c4cbccdb7a8b37ba8e26f2ba18b90ad22f5262
Sanghee <95578975+AhnSangHee@users.noreply.github.com>
2023-10-26 21:21:55 +0900

    Driver Selina (#48)

--- chessApp/King.swift
+++ chessApp/King.swift
@@ -7,13 +7,32 @@
 
 import Foundation
 
-class King {
-    let position: (Int, Int)
+struct King: Equatable {
+  let position: Position
+  let color: Color
+  
+  func availablePositions() -> [Position] {
+    position.moved(rank: 0, file: 1)
+    position.moved(rank: 1, file: 0)
+    position.moved(rank: 1, file: 1)
+    position.moved(rank: -1, file: 0)
+    position.moved(rank: 0, file: -1)
+    position.moved(rank: -1, file: -1)
+    position.moved(rank: 1, file: 0)
+    position.moved(rank: -1, file: 0)
+  }
 }
 
 enum Color {
-    case black
-    case white
+  case black
+  case white
 }
 
-
+struct Position: Equatable {
+  var rank: Int
+  var file: Int
+  
+  func moved(rank: Int, file: Int) -> Position {
+    return Position(rank: self.rank + rank, file: self.file + file)
+  }
+}
