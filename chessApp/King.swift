35204b472edab6133216de9e974d2271dd004a1f
Sanghee <dev.sanghee@gmail.com>
2023-10-26 21:59:30 +0900

    Driver Selina

--- chessApp/King.swift
+++ chessApp/King.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct King: Equatable {
-    let position: Position
+    var position: Position
     let color: Color
     
     func availablePositions() -> [Position] {
@@ -23,6 +23,16 @@ struct King: Equatable {
             position.moved(rank: -1, file: 1)
         ]
     }
+  
+  mutating func move(rank: Int, file: Int) -> Bool {
+    let newPosition = self.position.moved(rank: rank, file: file)
+    guard self.availablePositions().contains(newPosition) else {
+      return false
+    }
+    
+    self.position = newPosition
+    return true
+  }
 }
 
 enum Color {
@@ -31,10 +41,15 @@ enum Color {
 }
 
 struct Position: Equatable {
-    var rank: Int
-    var file: Int
+    let rank: Int
+    let file: Int
     
     func moved(rank: Int, file: Int) -> Position {
         return Position(rank: self.rank + rank, file: self.file + file)
+    }
+  
+    init?(rank: Int, file: Int) {
+        self.rank = rank
+        self.file = file
     }
 }
--- chessAppTests/KingTest.swift
+++ chessAppTests/KingTest.swift
@@ -9,16 +9,27 @@ import XCTest
 
 final class KingTest: XCTestCase {
 
-    
     func test_king이_생성됨() {
         let sut = King(position: Position(rank: 0, file: 0), color: .black)
         XCTAssertEqual(King(position: Position(rank: 0, file: 0), color: .black), sut)
     }
     
-    func test_king이_이동함() {
-        let sut = King(position: Position(rank: 0, file: 0), color: .black)
-        let newPosition = sut.position.moved(rank: 0, file: 1)
-        XCTAssertEqual(newPosition, Position(rank: 0, file: 1))
+    func test_king이_오른쪽으로_한칸_이동함() {
+        var sut = King(position: Position(rank: 0, file: 0), color: .black)
+        let didMove = sut.move(rank: 0, file: 1)
+        XCTAssertTrue(didMove)
+    }
+  
+    func test_King이_왼쪽으로_한칸_이동함() {
+        var sut = King(position: Position(rank: 5, file: 5), color: .white)
+        let didMove = sut.move(rank: 0, file: -1)
+        XCTAssertTrue(didMove)
+    }
+  
+    func test_King이_오른쪽이동으로_오른쪽_경계를_넘을수없음() {
+        var sut = King(position: Position(rank: 0, file: 7), color: .black)
+        let didMove = sut.move(rank: 0, file: 1)
+        XCTAssertFalse(didMove)
     }
     
     override func setUpWithError() throws {
