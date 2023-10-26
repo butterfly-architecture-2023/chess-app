b85361ef20865aae2c3b6449b55a23af4b963fbd
KYHyeon <run190822@gmail.com>
2023-10-26 21:08:09 +0900

    initial commit (#47)

---
+++ chessApp/King.swift
@@ -0,0 +1,19 @@
+//
+//  King.swift
+//  chessApp
+//
+//  Created by 현기엽 on 2023/10/26.
+//
+
+import Foundation
+
+class King {
+    let position: (Int, Int)
+}
+
+enum Color {
+    case black
+    case white
+}
+
+
---
+++ chessApp/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  chessApp
+//
+//  Created by 현기엽 on 2023/10/26.
+//
+
+import Foundation
+
+print("Hello, World!")
+
