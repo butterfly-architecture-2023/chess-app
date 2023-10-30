80f522d9eb6979c7a31e8af3cd064682c39ad8f8
Joy <50580583+khyeji98@users.noreply.github.com>
2023-10-30 10:58:37 +0900

    [joy] 짝프로그래밍 프로젝트 추가 (#52)
    * init project
    
    * feat: Board 객체 정의
    
    * feat: PieceColor enum 객체 및 케이스 정의
    
    * feat: Rank enum 객체 및 케이스 정의
    
    - Board 가로 행을 나타내는 객체
    
    * feat: File enum 객체 및 케이스 정의
    
    - Board 세로 열을 의미하는 객체
    
    * feat: Square 객체 정의
    
    - 체스 보드판의 각 칸을 의미하는 객체
    
    * feat: Piece 객체 정의
    
    * feat: Square에 피스 유무 확인 기능 추가
    
    * feat: 체스 보드판 초기 세팅 기능 추가
    
    - 각 체스 칸 및 좌표 세팅
    
    * feat: 피스 움직이는/옮기는 기능 추가
    
    - 피스를 옮기고 싶은 좌표의 피스 존재 여부와 색 구분하는 로직
    - 피스를 옮길 수 있는지 여부에 대한 값 반환
    
    * feat: 보드 초기 세팅시 rank 2,7에 각 피스(폰)이 위치하는 기능 추가
    
    * feat: Pawn 객체 및 Piece 프로토콜 정의
    
    * feat: 움직일 수 있는 좌표 반환 기능 추가
    
    * feat: 출력시 표현될 string 값 정의
    
    * feat: 체스 말 움직이는 기능 로직 수정
    
    - 진행상황 저장 타입 및 저장 로직 수정
    
    * feat: 현재 좌표에서 움직일 수 있는 좌표 반환 로직 추가
    
    * feat: File, Rank 값 정의 수정
    
    * test: Pawn 행마(전진) 기능 테스트코드 추가
    
    * test: 행마(전진) 기능 테스트코드 추가
    
    * 짝프로그래밍 프로젝트 추가

---
+++ PairChess/PairChess/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PairChess
+//
+//  Created by 김혜지 on 10/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PairChess/PairChess/PairChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  PairChessApp.swift
+//  PairChess
+//
+//  Created by 김혜지 on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct PairChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
