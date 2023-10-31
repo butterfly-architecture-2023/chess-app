57bc92779a3e5492f6f7372155d55c017c466c48
HongzCloud <jihong21511@gmail.com>
2023-11-01 03:05:21 +0900

    :white_check_mark: Piece 테스트 코드 수정

--- ChessGame/ChessGameTests/PieceTests.swift
+++ ChessGame/ChessGameTests/PieceTests.swift
@@ -11,19 +11,19 @@ import XCTest
 final class PieceTests: XCTestCase {
 
     func testMovablePositions_검은_폰의_위치가_file_0_rank_0_일_때_file_0_rank_1_이_포함된_배열_반환() {
-        let position = Position(file: 0, rank: 0)
-        let sut = BlackPawn()
-        let positions = sut.movablePositions(current: position)
-        let target = Position(file: 0, rank: 1)
+        let position = Position(file: 0, rank: 0)!
+        let pawn = BlackPawn()
+        let movablePositions = pawn.movablePositions(current: position)
+        let target = [Position(file: 0, rank: 1)]
 
-        XCTAssertTrue(positions.contains{ $0 == target })
+        XCTAssertEqual(movablePositions, target)
     }
 
     func testMovablePositions_하얀_폰의_위치가_file_7_rank_7_일_때_file_7_rank_6_이_포함된_배열_반환() {
-        let position = Position(file: 7, rank: 7)
+        let position = Position(file: 7, rank: 7)!
         let sut = WhitePawn()
         let positions = sut.movablePositions(current: position)
-        let target = Position(file: 7, rank: 6)
+        let target = Position(file: 7, rank: 6)!
 
         XCTAssertTrue(positions.contains{ $0 == target })
     }
