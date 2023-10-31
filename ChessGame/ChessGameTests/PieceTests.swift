5053045c8a0dc5e2a2f0157f2488b5d436dbd079
HongzCloud <jihong21511@gmail.com>
2023-11-01 03:31:22 +0900

    :white_check_mark: 비숍이 움직일 수 있는 Position 기능 테스트

--- ChessGame/ChessGameTests/PieceTests.swift
+++ ChessGame/ChessGameTests/PieceTests.swift
@@ -27,4 +27,46 @@ final class PieceTests: XCTestCase {
 
         XCTAssertTrue(positions.contains{ $0 == target })
     }
+
+    func testMovablePositions_검은_비숍의_위치가_file_0_rank_0_일_때_아래_오른쪽_대각선_위치들의_배열_반환() {
+        let position = Position(file: 0, rank: 0)!
+        let pawn = BlackBiship()
+        let movablePositions = pawn.movablePositions(current: position)
+
+        print(movablePositions)
+        
+        let target: [Position] = 
+        [
+            Position(file: 1, rank: 1)!,
+            Position(file: 2, rank: 2)!,
+            Position(file: 3, rank: 3)!,
+            Position(file: 4, rank: 4)!,
+            Position(file: 5, rank: 5)!,
+            Position(file: 6, rank: 6)!,
+            Position(file: 7, rank: 7)!
+        ]
+        
+        XCTAssertTrue(movablePositions.contains(target),
+                      "\(target)과 \n \(movablePositions)은 서로 다릅니다.")
+    }
+    
+    func testMovablePositions_하얀_비숍의_위치가_file_7_rank_7_일_때_위_왼쪽_대각선_위치들의_배열_반환() {
+        let position = Position(file: 7, rank: 7)!
+        let sut = WhiteBiship()
+        let movablePositions = sut.movablePositions(current: position)
+
+        let target: [Position] = 
+        [
+            Position(file: 6, rank: 6)!,
+            Position(file: 5, rank: 5)!,
+            Position(file: 4, rank: 4)!,
+            Position(file: 3, rank: 3)!,
+            Position(file: 2, rank: 2)!,
+            Position(file: 1, rank: 1)!,
+            Position(file: 0, rank: 0)!
+        ]
+
+        XCTAssertTrue(movablePositions.contains(target),
+                      "\(target)과 \n \(movablePositions)은 서로 다릅니다.")
+    }
 }
