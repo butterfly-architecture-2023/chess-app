53a43575ba829397667f3ffa6dfcb0acae342f5b
HongzCloud <jihong21511@gmail.com>
2023-11-01 03:52:58 +0900

    :sparkles: 퀸이 움직일 수 있는 Position 기능 추가

--- ChessGame/ChessGame/Pieces/Queen.swift
+++ ChessGame/ChessGame/Pieces/Queen.swift
@@ -11,7 +11,19 @@ struct BlackQueen: BlackPiece {
     static let score: Int = 9
     
     func movablePositions(current: Position) -> [Position] {
-        []
+        let bottomLeft = current.bottomLeftPositions()
+        let topLeft = current.topLeftPositions()
+        let bottomRight = current.bottomRightPositions()
+        let topRight = current.topRightPositions()
+        
+        let bottom = current.bottomPositions()
+        let top = current.topPositions()
+        let left = current.leftPositions()
+        let right = current.rightPositions()
+        
+        let positions = top + left + bottom + right + topRight + topLeft + bottomLeft + bottomRight
+        
+        return positions
     }
     
     static func initialPositions() -> [Position: Self] {
@@ -23,7 +35,19 @@ struct WhiteQueen: WhitePiece {
     static let score: Int = 9
     
     func movablePositions(current: Position) -> [Position] {
-        []
+        let bottomLeft = current.bottomLeftPositions()
+        let topLeft = current.topLeftPositions()
+        let bottomRight = current.bottomRightPositions()
+        let topRight = current.topRightPositions()
+        
+        let bottom = current.bottomPositions()
+        let top = current.topPositions()
+        let left = current.leftPositions()
+        let right = current.rightPositions()
+        
+        let positions = top + left + bottom + right + topRight + topLeft + bottomLeft + bottomRight
+        
+        return positions
     }
     
     static func initialPositions() -> [Position: Self] {
