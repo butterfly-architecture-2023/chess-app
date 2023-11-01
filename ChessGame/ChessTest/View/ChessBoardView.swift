6e66866de4de9f60b71799b7b143b42707635d50
camosss <camosss@naver.com>
2023-11-02 00:22:34 +0900

    feat: chessBoardView 생성 및 폴더링

---
+++ ChessGame/ChessTest/View/ChessBoardView.swift
@@ -0,0 +1,21 @@
+//
+//  ChessBoardView.swift
+//  ChessGame
+//
+//  Created by 강호성 on 11/1/23.
+//
+
+import UIKit
+
+final class ChessBoardView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+
+    }
+}
